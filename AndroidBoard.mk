cee1f5a6fb9fa79ee37f05a54cbecd2e3d9dd4e9
sub77 <sub77@ymail.com>
2016-05-10 11:07:26 -0500

    Update

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 # Compile Linux Kernel
 #----------------------------------------------------------------------
 
-include kernel/AndroidKernel.mk
+include kernel/motorola/msm8226/AndroidKernel.mk
 
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	$(transform-prebuilt-to-target)
